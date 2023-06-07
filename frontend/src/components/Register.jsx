3513fa26c560a3c1205a0c563d1bc45f0229b832
Brandon Minjares <bmminjar@ucsc.edu>
2023-06-07 14:03:39 -0700

    modified port

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -29,7 +29,7 @@ export default function Register() {
       password: data.get('password'),
     };
 
-    fetch(`https://filmbot.io/v0/register`, {
+    fetch(`https://oyster-app-bb9vv.ondigitalocean.app/v0/register`, {
       method: 'POST',
       body: JSON.stringify(user),
       headers: {
