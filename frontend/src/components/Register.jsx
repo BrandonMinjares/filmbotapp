237f1b22ca9678e572aa3216c01219dd5eca8baf
Brandon Minjares <bmminjar@ucsc.edu>
2023-06-08 18:10:57 -0700

    added environment variable for BaseUrl for http requests

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -29,7 +29,7 @@ export default function Register() {
       password: data.get('password'),
     };
 
-    fetch(`https://filmbot.io/filmbotapp-backend/v0/register`, {
+    fetch(`${process.env.REACT_APP_BASE_URL}/v0/register`, {
       method: 'POST',
       body: JSON.stringify(user),
       headers: {
